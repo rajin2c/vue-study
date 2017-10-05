121cf83785c8a71be81b9674fd6a0da68896bef2
renjistudy <renjithvk93@gmail.com>
2017-10-05 15:35:02 +0000

    Class binding: object syntax

--- main.js
+++ main.js
@@ -1,43 +1,6 @@
-let askMe = new Vue ({
-   el: '#askMe',
+let app = new Vue ({
+   el: '#app',
    data: {
-      question: '',
-      answer: 'ask your question',
-   },
-   
-   watch: {
-      question: function(question) {
-         this.answer = 'waiting for stop typing';
-         this.getAnswer();
-      }
-   },
-   
-   methods: {
-      getAnswer: _.debounce (
-         function() {
-            let hasQuestionMark = (this.question.indexOf('?') == -1) ? false : true ;
-         
-         if(!hasQuestionMark) {
-            this.answer = 'please put a question mark  ';
-            return;
-         }
-         
-         this.answer = 'thinking...';
-         let self = this;
-         
-         axios.get('https://yesno.wtf/api')
-         
-            .then(function(response) {
-               self.answer = _.upperCase(response.data.answer);
-            })
-            
-            .catch(function(error) {
-               self.answer = 'Unable to process.. Please try again later..';
-            });
-            
-            }
-      
-       , 1000)
+      isParaActive: false,
    }
-   
 });
