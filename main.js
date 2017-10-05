f11e9ea41ddf049b78c72c7a3d36c51397766741
rajin2c <rajin2c@gmail.com>
2017-10-05 15:14:57 +0000

    Watcher: random example 2

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-let ansme = new Vue({
-   el: '#ansme',
+let askMe = new Vue ({
+   el: '#askMe',
    data: {
       question: '',
-      answer: 'Please ask any question',
+      answer: 'ask your question',
    },
    
    watch: {
       question: function(question) {
-         this.answer = 'Waiting for stop typing...',
+         this.answer = 'waiting for stop typing';
          this.getAnswer();
       }
    },
    
    methods: {
-      getAnswer: _.debounce(
+      getAnswer: _.debounce (
          function() {
-            let hasQuestionMark = (this.question.indexOf('?') == -1) ? false : true;
-            
-            if (!hasQuestionMark) {
-               this.answer = 'Please put a question mark';
-               return;
-            }
-            
-            this.answer = 'Thinking...';
-            let self = this;
-            
-            axios.get('https://yesno.wtf/api')
-            
-               .then(function(response) {
-                  self.answer = _.upperCase(response.data.answer);
-               })
-               
-               .catch( function(error) {
-                  self.answer = 'Unable to process. Please try again later';
-               });
+            let hasQuestionMark = (this.question.indexOf('?') == -1) ? false : true ;
+         
+         if(!hasQuestionMark) {
+            this.answer = 'please put a question mark  ';
+            return;
          }
-      , 1000)
+         
+         this.answer = 'thinking...';
+         let self = this;
+         
+         axios.get('https://yesno.wtf/api')
+         
+            .then(function(response) {
+               self.answer = _.upperCase(response.data.answer);
+            })
+            
+            .catch(function(error) {
+               self.answer = 'Unable to process.. Please try again later..';
+            });
+            
+            }
       
+       , 1000)
    }
+   
 });
