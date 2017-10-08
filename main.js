55a0aa83e67e5ff901a39c60b35cb8e76e82509a
rajin2c <rajin2c@gmail.com>
2017-10-08 12:14:28 +0000

    Event handling : Click add example

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-Vue.component('wish', {
-   props: ['wish'],
-   template: '<li v-if="wish.id != 2">{{ wish.msg }}</li>',
-});
-
-let app = new Vue({
-   el: '#app',
+let test = new Vue ({
+   el: '#test',
    data: {
-      wishes: [
-         { id: 1, msg: 'Good morning' },
-         { id: 2, msg: 'Good afternone' },
-         { id: 3, msg: 'Good evening' },
-         { id: 4, msg: 'Good night' },
-      ],
-   },
+      counter: 0,
+   }
 });
