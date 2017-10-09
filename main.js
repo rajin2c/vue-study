eecd0770073eaeade0074ed9833e1b2382127484
renjistudy <renjithvk93@gmail.com>
2017-10-09 15:39:29 +0000

    Event handling: configure new key aliase

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+Vue.config.keyCodes.arrows = [37, 38, 39, 40];
+
 let keyboard = new Vue({
    el: '#keyboard',
    
    methods: {
       msg: function() {
-         alert('You are pressed enter!');
-      }
+         alert('You are pressed a arrow key!');
+      },
    }
 });
