283074f9755bceb99913375c136d68210bfbb58c
renjistudy <renjithvk93@gmail.com>
2017-10-16 15:51:05 +0000

    Local components

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 
-// Site
+// Components
+let header = {
+   template: '<div class="title"><h1>This is a heading</h1></div>',
+};
+
+let content = {
+   template: '<div><p>Content area</p></div>',
+};
+
+let footer = {
+   template: '<div><p>Copyright @ 2017</p></div>',
+};
+
+// Instance
 let site = new Vue({
    el: '#site',
+   components: {
+      'site-header': header,
+      'site-content':content,
+      'site-footer': footer,
+   },
 });
+
